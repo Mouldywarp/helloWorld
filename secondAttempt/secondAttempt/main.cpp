52424c045c1c6cbd0f4cb45026e39cb56be105f8
Aleckz <p03069522@myemail.dmu.ac.uk>
2015-01-13 22:06:01 +0000

    Buttttterz
    First commit for Alex branch, valuable work

--- secondAttempt/secondAttempt/main.cpp
+++ secondAttempt/secondAttempt/main.cpp
@@ -4,5 +4,11 @@
 void main()
 {
 	std::cout << "Alwight Dawlin\n";
-	std::cout << "Oh look at me changing a file and the committing it\n\n";
+	std::cout << "Oh look at me changing a file and then committing it\n\n";
+
+	// This outputs valuable info to the screen
+	for(int i = 0; i < 50; i++)
+	{
+		std::cout << "The only commiting you'll be doing is suicide!\n";
+	}
 }
