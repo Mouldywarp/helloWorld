07d8717fb4f58d392dfd946a776d90d9dd06e38f
Karclan <radier206@hotmail.co.uk>
2015-01-13 22:17:51 +0000

    Update to main and added class

---
+++ secondAttempt/secondAttempt/class.h
@@ -0,0 +1,13 @@
+#ifndef _CLASS_H_
+#define _CLASS_H_
+
+namespace mne
+{
+	int dicks = 44;
+}
+class Class
+{
+public:
+
+};
+#endif 
--- secondAttempt/secondAttempt/main.cpp
+++ secondAttempt/secondAttempt/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include "class.h"
 
-
+void dinosaur(int i)
+{
+	switch(i)
+	{
+	case 0:
+		std::cout<<"John Berverg";
+		break;
+	case 1:
+		std::cout<<"Avec Moi";
+		break;
+	default:
+		std::cout<<std::endl;
+	}
+	
+}
 void main()
 {
 	std::cout << "Alwight Dawlin\n";
 	std::cout << "Oh look at me changing a file and the committing it\n\n";
+	dinosaur(0);
+	dinosaur(1);
+	dinosaur(2);
+	dinosaur(2);
+	dinosaur(1);
+	dinosaur(3);
+	std::cout<<mne::dicks<<std::endl;
+	while(1);
 }
